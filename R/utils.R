bedf5c653b3deeaae778583e981f0e650bd64b5a
Wasin Pipattungsakul <wasin.pipattungsakul@adelaide.edu.au>
2025-06-10 13:58:46 +0930

    add test cases

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,20 @@ env_add_one <- function(env, key) {
     env[[key]] <- env[[key]] + 1
   }
 }
+
+pseudo_inverse = function(a_matrix, tolerance = 1e-10) {
+  matrix_name <- deparse(substitute(a_matrix))
+  svd_a <- svd(a_matrix)
+  rank_a <- sum(svd_a$d > tolerance)
+
+  # Moore-Penrose inverse (variance matrix)
+  if (rank_a > 0) {
+    return(
+      svd_a$v[, 1:rank_a] %*%
+        diag(1 / svd_a$d[1:rank_a]) %*%
+        t(svd_a$u[, 1:rank_a])
+    )
+  } else {
+    stop(paste0(matrix_name, " has rank 0 - design may be invalid"))
+  }
+}
