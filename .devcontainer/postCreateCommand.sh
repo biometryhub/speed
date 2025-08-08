bb9c6a8d7b5a30302fb2f3e55c8ea16039201bcc
Wasin Pipattungsakul <wasin.pipattungsakul@adelaide.edu.au>
2025-08-08 11:03:31 +0930

    update container to rocker

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,12 @@
 sudo apt-get -y update
 sudo apt-get install -y libudunits2-dev libgdal-dev libgeos-dev libproj-dev libglpk-dev
+
+# https://gist.github.com/sakethramanujam/faf5b677b6505437dbdd82170ac55322
+wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-6/gcc-6-base_6.4.0-17ubuntu1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-6/libgfortran3_6.4.0-17ubuntu1_amd64.deb
+
+sudo dpkg -i gcc-6-base_6.4.0-17ubuntu1_amd64.deb
+sudo dpkg -i libgfortran3_6.4.0-17ubuntu1_amd64.deb
+
+rm gcc-6-base_6.4.0-17ubuntu1_amd64.deb
+rm libgfortran3_6.4.0-17ubuntu1_amd64.deb
