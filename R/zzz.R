a2112a077f9cc2ce40ac2ff8d0910068c78dfa88
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2025-10-30 14:48:13 +1030

    Adding function to check current version vs GItHub version

---
+++ R/zzz.R
@@ -0,0 +1,35 @@
+.onAttach <- function(libname, pkgname) {
+  tryCatch({
+    # Get the GitHub raw URL for the DESCRIPTION file
+    github_desc_url <- "https://raw.githubusercontent.com/biometryhub/speed/refs/heads/main/DESCRIPTION"
+    
+    # Get current package version
+    current_version <- packageVersion(pkgname)
+    
+    # Try to read the remote DESCRIPTION file
+    remote_desc <- readLines(github_desc_url, warn = FALSE)
+    
+    # Extract version line
+    version_line <- grep("^Version:", remote_desc, value = TRUE)
+    
+    if (length(version_line) > 0) {
+      # Extract version number
+      remote_version <- sub("^Version:\\s*", "", version_line[1])
+      remote_version <- package_version(remote_version)
+      
+      # Compare versions
+      if (remote_version > current_version) {
+        packageStartupMessage(
+          sprintf(
+            "A newer version of %s is available on GitHub (installed: %s, available: %s).\nUpdate with: devtools::install_github(\"username/repo\")",
+            pkgname,
+            current_version,
+            remote_version
+          )
+        )
+      }
+    }
+  }, error = function(e) {
+    # Silently fail - no output if there's any error
+  })
+}
