3c83d91ea61b30738f97e95f06ee2dbe0d1e99e3
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2025-11-04 11:49:56 +1030

    Adding tests for startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,26 +2,26 @@
   tryCatch({
     # Get the GitHub raw URL for the DESCRIPTION file
     github_desc_url <- "https://raw.githubusercontent.com/biometryhub/speed/refs/heads/main/DESCRIPTION"
-    
+
     # Get current package version
-    current_version <- packageVersion(pkgname)
-    
+    current_version <- get_package_version(pkgname)
+
     # Try to read the remote DESCRIPTION file
-    remote_desc <- readLines(github_desc_url, warn = FALSE)
-    
+    remote_desc <- read_lines_wrapper(github_desc_url, warn = FALSE)
+
     # Extract version line
     version_line <- grep("^Version:", remote_desc, value = TRUE)
-    
+
     if (length(version_line) > 0) {
       # Extract version number
       remote_version <- sub("^Version:\\s*", "", version_line[1])
       remote_version <- package_version(remote_version)
-      
+
       # Compare versions
       if (remote_version > current_version) {
         packageStartupMessage(
           sprintf(
-            "A newer version of %s is available on GitHub (installed: %s, available: %s).\nUpdate with: devtools::install_github(\"username/repo\")",
+            "A newer version of %s is available on GitHub (installed: %s, available: %s).\nUpdate with: devtools::install_github(\"biometryhub/speed\")",
             pkgname,
             current_version,
             remote_version
@@ -32,4 +32,13 @@
   }, error = function(e) {
     # Silently fail - no output if there's any error
   })
+}
+
+# Wrapper functions for easier mocking in tests
+get_package_version <- function(pkg) {
+  utils::packageVersion(pkg)
+}
+
+read_lines_wrapper <- function(con, warn = TRUE) {
+  base::readLines(con, warn = warn)
 }
